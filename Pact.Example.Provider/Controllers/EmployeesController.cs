e8febeb5768d9dd0f5c64c296ad5b7f10ffe9272
bawaji94 <shabbirb@thoughtworks.com>
2019-04-15 19:26:02 +0530

    Initial Commit

---
+++ Pact.Example.Provider/Controllers/EmployeesController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Pact.Example.Provider.Models;
+
+namespace Pact.Example.Provider.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // GET api/employees
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return new List<Employee>
+            {
+                new Employee
+                {
+                    Id = "1",
+                    Name = "Bhagwan Ram: The God"
+                }
+            };
+        }
+
+        // GET api/employees/1
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            return new Employee
+            {
+                Id = "1",
+                Name = "Bhagwan Ram: The God"
+            };
+        }
+
+        // POST api/employees
+        [HttpPost]
+        public ActionResult<Employee> Post([FromBody] Employee employee)
+        {
+            return new Employee
+            {
+                Id = "1"
+            };
+        }
+
+        // PUT api/employees/1
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee employee)
+        {
+        }
+    }
+}
---
+++ Pact.Example.Provider/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace Pact.Example.Provider.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
